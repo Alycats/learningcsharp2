5156ff7e4022758e1b8c59ab9ca1c4634b810a2b
Alycat <Arc183@student.aru.ac.uk>
2023-01-19 15:55:11 +0000

    Bunch of Math

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ class Program //Write a C# program to print the sum of two numbers
         Console.Write("Enter the second number: ");
         secondvariable = Convert.ToDouble( Console.ReadLine() );
 
-        Console.WriteLine($"The sum of {firstvariable} and {secondvariable} is {firstvariable + secondvariable}");
+        Console.WriteLine($" {firstvariable} / {secondvariable} = {firstvariable / secondvariable}");
         Console.WriteLine($" {firstvariable} + {secondvariable} = {firstvariable + secondvariable}");
-        
+        Console.WriteLine($" {firstvariable} - {secondvariable} = {firstvariable - secondvariable}");
+        Console.WriteLine($" {firstvariable} * {secondvariable} = {firstvariable * secondvariable}");
+
         Console.ReadKey(true);
     }
 }
