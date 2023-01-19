85659827ecb23db3e90538fb126a1bb11097a2bc
Alycat <Arc183@student.aru.ac.uk>
2023-01-19 15:45:42 +0000

    User Input

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ class Program //Write a C# program to print the sum of two numbers
     {
         int firstvariable = 7;
         int secondvariable = 12;
+        Console.Write("Enter the first number: ");
+        firstvariable = Convert.ToInt32( Console.ReadLine() );
+        Console.Write("Enter the second number: ");
+        secondvariable = Convert.ToInt32( Console.ReadLine() );
 
         Console.WriteLine($"The sum of {firstvariable} and {secondvariable} is {firstvariable + secondvariable}");
         Console.WriteLine($" {firstvariable} + {secondvariable} = {firstvariable + secondvariable}");
