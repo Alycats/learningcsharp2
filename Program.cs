1265e9122c9e102f0d8b326bb3a32a7fb92152db
Alycat <Arc183@student.aru.ac.uk>
2023-01-19 15:20:47 +0000

    7+12 Output Sum

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace learningcsharp2;
-class Program
+class Program //Write a C# program to print the sum of two numbers 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(7+12);
+        Console.ReadKey(true);
     }
 }
