65a7f094bf8732171101815c639965a9238aca65
Alycat <Arc183@student.aru.ac.uk>
2023-01-19 15:28:23 +0000

    Change numbers

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ class Program //Write a C# program to print the sum of two numbers
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(7+12);
+        int firstvariable = 7;
+        int secondvariable = 12;
+
+        Console.WriteLine(firstvariable + secondvariable);
         Console.ReadKey(true);
     }
 }
