179250afc40e6d729d3f7ca7446de70d0e05a91f
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 11:55:44 +0000

    Nothing new just set up for more

--- Program.cs
+++ Program.cs
@@ -7,21 +7,12 @@ class Program //Write a C# program  that takes a number as input and then displa
 
         Console.Write("Enter the first number: ");
         firstvariable = Convert.ToDouble( Console.ReadLine() );
-
-
-        Console.WriteLine($"{firstvariable}*0 = {(firstvariable)*0}");
-        Console.WriteLine($"{firstvariable}*1 = {(firstvariable)*1}");
-        Console.WriteLine($"{firstvariable}*2 = {(firstvariable)*2}");
-        Console.WriteLine($"{firstvariable}*3 = {(firstvariable)*3}");
-        Console.WriteLine($"{firstvariable}*4 = {(firstvariable)*4}");
-        Console.WriteLine($"{firstvariable}*5 = {(firstvariable)*5}");
-        Console.WriteLine($"{firstvariable}*6 = {(firstvariable)*6}");
-        Console.WriteLine($"{firstvariable}*7 = {(firstvariable)*7}");
-        Console.WriteLine($"{firstvariable}*8 = {(firstvariable)*8}");
-        Console.WriteLine($"{firstvariable}*9 = {(firstvariable)*9}");
-        Console.WriteLine($"{firstvariable}*10 = {(firstvariable)*10}");
-        Console.WriteLine($"{firstvariable}*11 = {(firstvariable)*11}");
-        Console.WriteLine($"{firstvariable}*12 = {(firstvariable)*12}");
+        
+         Console.WriteLine($"{firstvariable}{firstvariable}{firstvariable}{firstvariable} ");
+         Console.WriteLine($"{firstvariable}  {firstvariable} ");
+         Console.WriteLine($"{firstvariable}  {firstvariable} ");
+         Console.WriteLine($"{firstvariable}  {firstvariable} ");
+         Console.WriteLine($"{firstvariable}{firstvariable}{firstvariable}{firstvariable} ");
        
 
         Console.ReadKey(true);
