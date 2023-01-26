5a0725139c919a2decdbc53859f6fe40a1f9cfb6
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 09:15:45 +0000

    Swap Numbers

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿namespace learningcsharp2;
-class Program //Write a C# program to print the sum of two numbers 
+class Program //Write a C# program that takes four numbers as input to calculate and print the average  
 {
     static void Main(string[] args)
     {
-        int firstvariable = 5;
-        int secondvariable = 6;
-        Console.Write("Enter the first number: ");
-        firstvariable = Convert.ToInt32( Console.ReadLine() );
-        Console.Write("Enter the second number: ");
-        secondvariable = Convert.ToInt32( Console.ReadLine() );
+        double firstvariable = 5;
+        double secondvariable = 6;
+        double thirdvariable = 7;
+        double forthvariable = 8;
 
-        Console.WriteLine($" {secondvariable} {firstvariable}");
+        Console.Write("Enter the first number: ");
+        firstvariable = Convert.ToDouble( Console.ReadLine() );
+
+        Console.Write("Enter the second number: ");
+        secondvariable = Convert.ToDouble( Console.ReadLine() );
+
+        Console.Write("Enter the third number: ");
+        thirdvariable = Convert.ToDouble( Console.ReadLine() );
+
+        Console.Write("Enter the forth number: ");
+        forthvariable = Convert.ToDouble( Console.ReadLine() );
+
+        Console.WriteLine($" The average of {firstvariable} , {secondvariable} , {thirdvariable} , {forthvariable} = {(firstvariable * secondvariable * thirdvariable * forthvariable)/4}");
 
         Console.ReadKey(true);
     }
