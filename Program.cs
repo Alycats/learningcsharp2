ca2cadfb8188d0d47046599cb6a9e1617f2ea9d5
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 09:47:01 +0000

    Box 3x5

--- Program.cs
+++ Program.cs
@@ -1,26 +1,20 @@
 ﻿namespace learningcsharp2;
-class Program //Write a C# program that takes four numbers as input to calculate and print the average  
+class Program //Write a C# program  that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit
 {
     static void Main(string[] args)
     {
         double firstvariable = 5;
-        double secondvariable = 6;
-        double thirdvariable = 7;
-        double forthvariable = 8;
+   
 
         Console.Write("Enter the first number: ");
         firstvariable = Convert.ToDouble( Console.ReadLine() );
 
-        Console.Write("Enter the second number: ");
-        secondvariable = Convert.ToDouble( Console.ReadLine() );
 
-        Console.Write("Enter the third number: ");
-        thirdvariable = Convert.ToDouble( Console.ReadLine() );
-
-        Console.Write("Enter the forth number: ");
-        forthvariable = Convert.ToDouble( Console.ReadLine() );
-
-        Console.WriteLine($" The average of {firstvariable} , {secondvariable} , {thirdvariable} , {forthvariable} = {(firstvariable * secondvariable * thirdvariable * forthvariable)/4}");
+        Console.WriteLine($"{firstvariable}{firstvariable}{firstvariable}{firstvariable} ");
+        Console.WriteLine($"{firstvariable}  {firstvariable} ");
+        Console.WriteLine($"{firstvariable}  {firstvariable} ");
+        Console.WriteLine($"{firstvariable}  {firstvariable} ");
+        Console.WriteLine($"{firstvariable}{firstvariable}{firstvariable}{firstvariable} ");
 
         Console.ReadKey(true);
     }
